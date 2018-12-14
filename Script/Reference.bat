2381abb5c59cfdf36553e88b9af5d8270d658b0f
Iguichi <45214414+Iguichi@users.noreply.github.com>
2018-12-14 12:38:03 +0100

    Update Reference.bat
    Reference

--- Script/Reference.bat
+++ Script/Reference.bat
@@ -1,17 +1,20 @@
 
+
+
+
+
 cd "/home/nas02a/etudiants/inf/uapv1900103/Donnees_itinerantes/Bureau/Papers"
 mkdir text
 mkdir info
 mkdir information
 for i in *.pdf
 do
-	echo $i
-	pdftotext -layout -f 1 -raw "$i" "text/$i.txt"
-	sed -n '/Abstract/,/Introduction/p' "text/$i.txt" >> "info/$i.txt"
-	sed '$d' "info/$i.txt" >> "information/$i.txt"
-	rm -rf info
-	rm -rf text
+echo 
+pdftotext -layout -raw "" "text/.txt"
+sed -n '/References/,/aaa/P' "text/.txt" >> "info/.txt"
+sed -n '/REFERENCES/,/aaa/P' "text/.txt" >> "info/.txt"
 
 
-	 
+ 
 done
+
